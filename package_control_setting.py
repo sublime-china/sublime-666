a5412e49d6780dc5e9222e95ecec26c3b78cdbb2
Floyda <floyda@163.com>
2019-03-27 12:32:25 +0800

    解决PC配置文件中, json字段不存在的Bug.

--- package_control_setting.py
+++ package_control_setting.py
@@ -8,22 +8,24 @@ REPOSITORIES_URL = 'https://github.com/sublime-china'
 SETTINGS_NAME = 'Package Control.sublime-settings'
 
 
-def deal_setting(name):
+def deal_setting(name, default):
     def decorator(func):
         def wrapper():
             s = sublime.load_settings(SETTINGS_NAME)
             params = s.get(name, None)
-            if params != None:
+            if params is None:
+                s.set(name, default)
+            else:
                 func(params)
                 s.set(name, params)
-                sublime.save_settings(SETTINGS_NAME)
+            sublime.save_settings(SETTINGS_NAME)
 
         return wrapper
 
     return decorator
 
 
-@deal_setting('channels')
+@deal_setting('channels', [CHANNELS_URL])
 def modify_channels(params):
     if ORIGIN_CHANNELS_URL in params:
         params.remove(ORIGIN_CHANNELS_URL)
@@ -31,13 +33,13 @@ def modify_channels(params):
         params.append(CHANNELS_URL)
 
 
-@deal_setting('repositories')
+@deal_setting('repositories', [REPOSITORIES_URL])
 def modify_repositories(params):
     if REPOSITORIES_URL not in params:
         params.append(REPOSITORIES_URL)
 
 
-@deal_setting('channels')
+@deal_setting('channels', [ORIGIN_CHANNELS_URL])
 def restore_channels(params):
     if CHANNELS_URL in params:
         params.remove(CHANNELS_URL)
@@ -45,7 +47,7 @@ def restore_channels(params):
         params.append(ORIGIN_CHANNELS_URL)
 
 
-@deal_setting('repositories')
+@deal_setting('repositories', [])
 def restore_repositories(params):
     if REPOSITORIES_URL in params:
         params.remove(REPOSITORIES_URL)
